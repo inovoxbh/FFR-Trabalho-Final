02692022dab0867bf954d51cd05dd23720641b66
Bruno G Alcantara <inovox@gmail.com>
2020-03-28 12:34:31 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { reducers } from "./reducers";
+
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    rootElement
+);
+
+// conteúdo original
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -11,7 +37,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
