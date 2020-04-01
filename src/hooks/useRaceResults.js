28da163e7398a1e411250e72cbc358144062f0b2
Bruno G Alcantara <inovox@gmail.com>
2020-04-01 20:45:42 -0300

    implementacao do hook useRaceResults

---
+++ src/hooks/useRaceResults.js
@@ -0,0 +1,20 @@
+import {useState,useEffect} from "react";
+import {f1Services} from "../services/f1Services.js";
+
+export const useRaceResults = (props) => {
+    const [raceResults,setRaceResults] = useState([]);
+
+    const getRaceResults= async () => {
+      const r = await f1Services.getRaceResult(props);
+      
+      const responseRaceResults =r.data.MRData.RaceTable.Races.Results;
+
+      setRounds(setRaceResults);
+    };
+
+    useEffect(() => {
+      getRaceResults();
+    },[props]);
+
+    return raceResults;
+};
